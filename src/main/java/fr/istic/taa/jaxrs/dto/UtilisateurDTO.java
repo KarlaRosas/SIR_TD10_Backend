b32274625fce525e8f75c5f9a86fd639a7714760
KarlaRosas19 <49324882+KarlaRosas19@users.noreply.github.com>
2021-04-06 11:27:51 +0200

    Test

---
+++ src/main/java/fr/istic/taa/jaxrs/dto/UtilisateurDTO.java
@@ -0,0 +1,14 @@
+package fr.istic.taa.jaxrs.dto;
+
+public class UtilisateurDTO {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
